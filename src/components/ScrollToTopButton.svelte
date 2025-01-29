791d5bbba0f46cf37bc284aeefdd8b7fb5db5be6
DonisPan <donisvasek@gmail.com>
2025-01-29 18:44:10 +0100

    make the scroll back to top button a component

---
+++ src/components/ScrollToTopButton.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }
+</script>
+
+<button class="scroll-to-top" on:click={scrollToTop} aria-label="Scroll to top">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
+    </svg>
+</button>
+
+<style>
+    .scroll-to-top {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        width: 50px;
+        height: 50px;
+        border: none;
+        border-radius: 50%;
+        background-color: var(--tekhelet);
+        color: white;
+        font-size: 1.5rem;
+        font-weight: bold;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+        transition: transform 0.3s ease, box-shadow 0.3s ease, background-color 0.3s ease;
+    }
+
+    .scroll-to-top:hover {
+        background-color: var(--ultra-violet);
+        transform: scale(1.1);
+        box-shadow: 0 6px 12px rgba(0, 0, 0, 0.4);
+    }
+
+    .scroll-to-top:active {
+        transform: scale(1);
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
     import RecipeCard from "../components/RecipeCard.svelte";
     import {onMount} from "svelte";
+    import ScrollToTopButton from "../components/ScrollToTopButton.svelte";
     export let data: { recipes: { id: bigint, name: string, image: string, difficulty: string }[] };
 
-    let isVisible = false;
+    let scrollToTopVisible = false;
 
     onMount(() => {
         const handleScroll = () => {
-            isVisible = window.scrollY > 300;
+            scrollToTopVisible = window.scrollY > 320;
         };
 
         window.addEventListener('scroll', handleScroll);
     });
-
-    function scrollToTop() {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-        });
-    }
 
     let searchQuery = '';
     let filteredRecipes = data.recipes;
@@ -32,12 +26,8 @@
     }
 </script>
 
-{#if isVisible}
-    <button class="scroll-to-top" on:click={scrollToTop} aria-label="Scroll to top">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
-        </svg>
-    </button>
+{#if scrollToTopVisible}
+    <ScrollToTopButton />
 {/if}
 
 <div class="search-container">
@@ -97,37 +87,6 @@
         width: 100%;
         margin: 0;
         align-items: center;
-    }
-
-    .scroll-to-top {
-        position: fixed;
-        bottom: 20px;
-        right: 20px;
-        width: 50px;
-        height: 50px;
-        border: none;
-        border-radius: 50%;
-        background-color: var(--tekhelet);
-        color: white;
-        font-size: 1.5rem;
-        font-weight: bold;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-        transition: transform 0.3s ease, box-shadow 0.3s ease, background-color 0.3s ease;
-    }
-
-    .scroll-to-top:hover {
-        background-color: var(--ultra-violet);
-        transform: scale(1.1);
-        box-shadow: 0 6px 12px rgba(0, 0, 0, 0.4);
-    }
-
-    .scroll-to-top:active {
-        transform: scale(1);
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
     }
 
     @media (max-width: 1400px) {
