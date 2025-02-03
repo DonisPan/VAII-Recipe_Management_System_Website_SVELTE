ea4bce479f5a1014b2369047b14de47235922a8c
DonisPan <donisvasek@gmail.com>
2025-02-03 16:09:44 +0100

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		cors: true,
-		proxy: {
-			'/api': {
-				target: process.env.VITE_SUPABASE_URL,
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/api/, '')
-			}
-		}
-	}
+	// server: {
+	// 	cors: true,
+	// 	proxy: {
+	// 		'/api': {
+	// 			target: process.env.VITE_SUPABASE_URL,
+	// 			changeOrigin: true,
+	// 			secure: false,
+	// 			rewrite: (path) => path.replace(/^\/api/, '')
+	// 		}
+	// 	}
+	// }
 });
 
